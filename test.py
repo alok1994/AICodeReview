51098491f4026530a16762f8432095192cd61806
Alok Kumar <verma.alok606@gmail.com>
2024-11-04 09:57:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ Class B(A):
     def __init__(self, secondName):
         self.secondName = secondName
         A.__init__(self, firstName)
+    def printFullName(self):
+        print('Full name:', self.firstName, self.secondName)
 
 a = A()
 a.printName()
