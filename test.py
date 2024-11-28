2b1754afcc4d0d0c9946d9dce2c27afe33cbedf8
Alok Kumar <verma.alok606@gmail.com>
2024-11-28 16:29:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,8 @@ Class B(A):
     def __init__(self, secondName):
         self.secondName = secondName
         A.__init__(self, firstName)
-
+class C(B):
+    def _init__(self):
+        
 a = A()
 a.printName()
